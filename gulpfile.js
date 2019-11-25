53a53c8531794701376c9d7a0625d4a2afa84c99
AlmeidaFabioO <fabio.633@hotmail.com>
2019-11-25 10:15:27 -0300

    configurando o gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const { src, dest, parallel } = require('gulp');
+const rename = require("gulp-rename");
+const minifyJs = require("gulp-uglify");
+const minifyCss = require('gulp-uglifycss');
+const image = require('gulp-image');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const cssImport = require('gulp-cssimport');
+
+function base(){
+    return src('src/templates/*.html')
+    .pipe(dest('public/'));
+}
+
+function javascript(){
+    return src('src/js/*.js')
+    .pipe(babel({
+        presets:['@babel/env']
+    }))
+    .pipe(minifyJs())
+    .pipe(rename({extname:'.min.js'}))
+    .pipe(dest('public/assets/js/'));
+}
+
+function css(){
+    return src('src/css/*.css')
+    .pipe(cssImport())
+    .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(rename({extname:'.min.css'}))
+    .pipe(dest('public/assets/css/'));
+}
+
+function imagens(){
+    return src('src/images/*.{jpg,png}')
+    .pipe(image())
+    .pipe(dest('public/images/'));
+}
+
+exports.default = parallel(base, javascript, css, imagens);
